ee5155e14ce6f985abe52bf15b494ba6eee9e796
Juan Patino <juan9patino9@gmail.com>
2022-01-27 19:35:18 -0500

    Initial commit

---
+++ src/hw1.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+import java.math.*;
+import java.io.*;
+
+public class hw1 {
+
+    public static void main(String[] args)throws IOException {//terrain.png, mpp.txt, redOut.png
+
+        File dict = new File(args[0]);
+        String word1 = args[1];
+        String word2 = args[2];
+
+    }
+}
